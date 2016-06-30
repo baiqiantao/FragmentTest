5c23468b71b81a301cd93e4f7de204191a216857
包青天 <baiqiantao@sina.com>
2016-06-30 12:52:47 +0800

    Update SingleFragmentActivity.java

--- src/com/bqt/fragment/demo/SingleFragmentActivity.java
+++ src/com/bqt/fragment/demo/SingleFragmentActivity.java
@@ -22,7 +22,7 @@ public abstract class SingleFragmentActivity extends Activity {
 		FragmentManager fm = getFragmentManager();
 		Fragment fragment = fm.findFragmentById(R.id.id_fragment_container);
 
-		if (fragment == null) {
+		if (savedInstanceState==nul) {//核心代码
 			//为什么需要判断是否为null？
 			//当【Activity】因为配置发生改变（如屏幕旋转）或者因内存不足被系统杀死，造成被重新创建时，我们的【fragment】会被保存下来
 			//虽然会创建新的【fm】，但是新的fm会首先去获取保存下来的fragment队列，重建fragment队列，从而可以恢复之前的状态。
