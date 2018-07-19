5ddb3cfcae463aa6d81f8a8b69dc60f5e1a1ae2e
baiqiantao <baiqiantao@sina.com>
2018-07-19 21:28:42 +0800

    产品B

---
+++ app/src/productB/java/com/bqt/test/SecondActivity.java
@@ -0,0 +1,11 @@
+package com.bqt.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_b);
+	}
+}
